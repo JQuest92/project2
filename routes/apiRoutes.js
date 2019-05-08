2e96450b25991cb9d12e3d9fa01dcd2dba99a12b
Rob Suttles <43940576+rsuttles58@users.noreply.github.com>
2019-05-08 16:04:53 -0500

    Restaurant Data Sequelize and API endpoint (#24)
    * Built Restaurant sequelize and the restaurant api endpoint
    
    * Restaurant sequelize and api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all restaurants
+  app.get("/api/restaurants", function(req, res) {
+    db.Restaurant.findAll({}).then(function(dbRestaurants) {
+      res.json(dbRestaurants);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
