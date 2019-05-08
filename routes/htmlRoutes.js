08cd670dbd5745fc3b263a4eedf3496e1ee60576
rsuttles58 <robert.suttles58@gmail.com>
2019-05-07 22:24:31 -0500

    Built Restaurant sequelize and the restaurant api endpoint

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Restaurant.findAll({}).then(function(dbRestaurants) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbRestaurants
       });
     });
   });
