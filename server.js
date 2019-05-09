67ced5802acf620e155391cbc65d1069dd95f11c
John Westbrooks <31968238+JQuest92@users.noreply.github.com>
2019-05-09 15:13:33 -0500

    Auth (#30)
    * Added authentication. Click on the login tab at the top to begin the process of creating an account. User fake emails and simple passes for ease.
    
    * fixed routing issue with sign-in link to create an account.
    
    * added id to user model. took out first and last names because mvp.
    
    * accidently tracked my config.json, removing my burner password

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-require("dotenv").config();
+//require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+//
 
 var db = require("./models");
 
@@ -11,6 +16,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
