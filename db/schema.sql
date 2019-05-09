2a5330b929a211be802e5df17d8a152c74bf0af0
denise martin <denise.martin@vanderbilt.edu>
2019-05-09 09:49:21 -0500

    modified config, schema styles htmlRoutes index user

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
+DROP DATABASE IF EXISTS hot_chicken;
 create database hot_chicken;
 use hot_chicken;
 
-
-create table review(
+DROP TABLE IF EXISTS reviews;
+create table reviews(
 restaurantName VARCHAR(500) NOT NULL,
 restaurantAddress VARCHAR(100) NOT NULL,
 userFirstName VARCHAR(100) NOT NULL,
@@ -14,32 +15,56 @@ comment VARCHAR(500) NOT NULL,
 PRIMARY KEY(user_id, restaurant_id) 
 );
 
+INSERT INTO reviews(restaurantName,restaurantAddress,userFirstName,userLastName, 
+            user_id,restaurant_id,score,comment)
+        VALUES("Pollo Loco", "Near you", "Alex", "Smith", 1, 1, 3, "Crazy chicken"),
+                ("Pollo rico", "Far from you", "Pedro", "Rodriguez", 2, 2, 2, "Too far");
+
+SELECT * FROM reviews;
 
 
-create table restaurant
-(
+
+DROP TABLE IF EXISTS restaurants;
+create table restaurants(
 name VARCHAR(500) NOT NULL,
 address VARCHAR(100) NOT NULL,
 city VARCHAR(100) default "Nashville",
 state VARCHAR(2) default "TN",
+zip INT,
 latitude DECIMAL(10,8),
 longitude DECIMAL(11,8),
 phone VARCHAR(12),
 website VARCHAR(200),
 facebook VARCHAR(200),
+createdAt VARCHAR(30),
+updatedAt VARCHAR(30),
 id INT AUTO_INCREMENT,
 Primary Key(id)
 );
 
-create table user
+INSERT INTO restaurants(name, address)
+            VALUES("Pollo Loco", "Near you"),
+                    ("Pollo rico", "Far from you");
+
+SELECT * FROM restaurants;
+
+
+DROP TABLE IF EXISTS users;
+create table users
 (
 firstName VARCHAR(100) NOT NULL,
 lastName VARCHAR(100) NOT NULL,
 email VARCHAR(250) NOT NULL,
-id INT UAUTO_INCREMENT,
+id INT AUTO_INCREMENT,
 code VARCHAR(250), /*possible password field*/
 PRIMARY KEY(id)
 );
+
+INSERT INTO users(firstName,lastName,email)
+        VALUES("Alex", "Smith", "alexsmith@test.com"),
+                ("Pedro", "Rodriguez", "pedrorodriquez@test.com");
+
+SELECT * FROM users;
 
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,14 +11,23 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load index page
+  app.get("/user", function(req, res) {
+    // Here you need code to get user
+    res.render("user");
   });
+
+  app.get("/example", function(req, res) {
+    res.render("example");
+  });
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
