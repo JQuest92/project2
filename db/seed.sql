6ae00d803082756dc7afec855147a5fe0e001a9a
Rob Suttles <43940576+rsuttles58@users.noreply.github.com>
2019-05-10 14:38:43 -0500

    Rob (#34)
    * Built Restaurant sequelize and the restaurant api endpoint
    
    * Restaurant sequelize and api
    
    * Adding restaurant sequelize
    
    * Fixed restaurant sequelize model.
    
    * Fixed restaurant model v2
    
    * Update to api and html routing
    
    * Small patch for Sequelize and ===== issue.
    
    * Created seed file and other minor changes

---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO restaurants
+    (name, address, city, state, zip, latitude, longitude, phone, website, facebook)
+VALUES
+    ("Slow Burn Hot Chicken", "495 Myatt Drive", "Madison", "TN", "37115", 36.27877, -86.69017, "629-202-7407", "https://www.slowburnhotchicken.com/#/", "https://www.facebook.com/SlowBurnHotChicken/"),
+    ("Prince's Hot Chicken Shack", "123 Ewing Drive", "Nashville", "TN", "37207", 36.2298, -86.76077, "615-810-9388", "https://www.princeshotchicken.com/", "https://www.facebook.com/PrincesHotChicken/"),
+    ("Prince's Hot Chicken Shack-South", "5814 Nolensville Pike #110", "Nashville", "TN", "37211", 36.04371, -86.71239, "615-810-9388", "https://www.princeshotchicken.com/", "https://www.facebook.com/PrincesHotChickenSouth/"),
+    ("Scoreboard Bar & Grill", "2408 Music Valley Drive", "Nashville", "TN", "37214", 36.22074, -86.69386, 615-883-3866, , "https://www.facebook.com/ScoreboardNashville/"),
+    ("400 Degrees Hot Chicken", "3704 Clarksville Pike", "Nashville", "TN", "37218", 36.19855, -86.83752, 615-244-4467, "http://www.400degreeshotchicken.com/", "https://www.facebook.com/400DegreesHotChicken/"),
+    ("PepperFire Hot Chicken", "1000 Gallatin Ave C", "Nashville", "TN", "37206", 36.1886, -86.7467, "615-582-4824", "https://www.pepperfirehotchicken.com/", "https://www.facebook.com/pepperfirehotchicken/"),
+    ("Bolton's Spicy Chicken & Fish", "624 Main Street #B", "Nashville", "TN", "37206", 36.17399, -86.76135, "615-254-8015", "https://www.boltonsspicy.com/", "https://www.facebook.com/BoltonsSpicyChickenandFish/"),
+    ("Hattie B's Hot Chicken-Midtown", "112 19th Ave S", "Nashville", "TN", "37203", 36.15296, -86.79765, "615-678-4794", "https://hattieb.com/", "https://www.facebook.com/HattieBsMidtownNashville/"),
+    ("Hattie B's Hot Chicken-Nashville West", "5209 Charlotte Ave", "Nashville", "TN", "37209", 36.15158, -86.85083, 615-712-7137, "https://hattieb.com/", "https://www.facebook.com/HattieBsCharlotteAve/"),
+    ("Hattie B's Hot Chicken-Melrose", "2222 8th Ave S", "Nashville", "TN", "37204", 36.12893, -86.77758, "615-970-3010", "https://hattieb.com/", "https://www.facebook.com/Hattiebmelrose/"),
+    ("Party Fowl", "719 8th Avenue South", "Nashville", "TN", "37203", 36.14987, -86.77948, "615-624-8255", "https://www.partyfowl.com/", "https://www.facebook.com/Partyfowlnashville/"),
+    ("Bishop's", "3065 Mallory Lane", "Franklin", "TN", "37067", 35.94385, -86.82115, "615-771-9432", "https://www.bishopsmeat3.com/", ),
+    ("Big Shake's Hot Chicken & Fish", "1203 Murfreesboro Road #185", "Franklin", "TN", "37064", 35.91738, -86.83117, "615-988-9335x2", "https://www.bigshakeshotchicken.com/", "https://www.facebook.com/bigshakeshotchickenFranklin/"),
+    ("Puckett's Grocery & Restaurant", "500 Church Street", "Nashville", "TN", "37219", 35.45284, -86.80035, "615-770-2772", "https://www.puckettsgro.com/", "https://www.facebook.com/pucketts5thchurch/"),
+    ("Puckett's Grocery & Restaurant", "120 4th Avenue South", "Franklin", "TN", "37064", 35.92376, -86.86999, "615-794-5527", "https://www.puckettsgro.com/", "https://www.facebook.com/downtown.franklin/"),
+    ("Helen's Hot Chicken-Nashville", "1801 Jefferson Street #101", "Nashville", "TN", "37208", 36.16906, -86.80721, "629-203-7971", "http://www.helenshotchicken.com/home", "https://www.facebook.com/Helens-Hot-Chicken-812290895450008/?__tn__=%2Cd%2CP-R&eid=ARBR_IB5Zop1Ysub3rewOuMNgf8HzyYRTjG7pZbUJ-2LY11UfhDFqpu0w3GrWzkDZGrZhd7tCbPtaqgE"),
+    ("The Stillery-Downtown", "113 2nd Avenue N", "Nashville", "TN", "37201", 36.16226, -86.77571, "615-942-8080", "http://www.stillerynashville.com/", "https://www.facebook.com/eatthestillery/"),
+    ("The Stillery-Midtown", "1921 Broadway", "Nashville", "TN", "37203", 36.14953, -86.79774, "615-760-5158", "http://www.stillerynashville.com/", "https://www.facebook.com/StilleryMidtown/"),
+    ("The Farm House", "210 Almond Street", "Nashville", "TN", "37201", 36.15777, -86.77428, "615-522-0688", "https://www.thefarmhousetn.com", "https://www.facebook.com/TheFarmHouseSoBro/"),
+    ("BJ Hot Chicken", "818 Murfreesboro Pike", "Nashville", "TN", "37217", 36.13544, -86.72366, "615-866-9365", , "https://www.facebook.com/BJHOTCHICK/?__tn__=%2Cd%2CP-R&eid=ARAB47pM7mBJgKpV9EDHEykcHsgAIVNXqwd9R6_sKu79Ik9TvBPBTxddCK5ad_Dar2fmDIgGeTcEUD5b"),
+    ("Tenn Sixteen Food & Drink Company", "1016 Woodland Street", "Nashville", "TN", "37206", 36.17719, -86.75036, "615-227-6313", , "https://www.facebook.com/Tenn16/"),
+    ("Acme Feed & Seed", "101 Broadway", "Nashville", "TN", "37203", 36.16187, -86.77432, "615-915-0888", "https://www.acmefeedandseed.com/menus", "https://www.facebook.com/pages/Acme-Feed-Seed/1650764368494429")
