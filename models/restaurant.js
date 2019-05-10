fbb6bf1f0af65f6796634cb11600f86268627a9c
rsuttles58 <robert.suttles58@gmail.com>
2019-05-09 22:32:41 -0500

    Created seed file and other minor changes

--- models/restaurant.js
+++ models/restaurant.js
@@ -15,7 +15,7 @@
 // };
 
 module.exports = function(sequelize, DataTypes) {
-  var Restaurant = sequelize.define("restaurant", {
+  var Restaurant = sequelize.define("Restaurant", {
     name: {
       type: DataTypes.STRING,
       field: "name"
