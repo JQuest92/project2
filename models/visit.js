711affef1293fa6ca9967185ec528981fcdf413c
John Westbrooks <31968238+JQuest92@users.noreply.github.com>
2019-05-10 20:02:09 -0500

    my rough draft for a working demo and proof-of-concept. The missing piece is getting the buttons to associate with the appriopriate info. Right now, they all have the id 22. (#38)
    nice work - thanks John!

---
+++ models/visit.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    var Visit = sequelize.define("Visit", {
+      restaurantName: {
+        type: DataTypes.STRING,
+        field: "restaurantName"
+      },
+      userName: {
+        type: DataTypes.STRING,
+        field: "userName"
+      }
+    },
+    {
+      freezeTableName: true
+    });
+    return Visit;
+  };
+  
