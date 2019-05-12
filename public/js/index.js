4eec3d611fce92bb6b00e0315278097716619935
Brian Storey <bwillstorey@gmail.com>
2019-05-12 14:25:36 -0500

    Mapping (#41)
    * refactor map API, adding custom icon, animation - both still WIPs
    
    * custom icon working
    
    * marker animation progress, only last marker animating
    
    * fixed css map display issue
    
    * updating initMap with info window logic. still wip
    
    * updates to info window formatting
    
    * map functionality added: info windows working, animations working
    
    * minor edits to info window styling and css map comments
    
    * removing config.json pwd
    
    * adding restaurant db call to index.js. minor linting updates to various files
    
    * updated info window to remove mouseout function
    
    * patched html route syntax bug
    
    * pulling data from local db
    
    * fixed infoWindow text color bug
    
    * removing local db pwd
    
    * apiroutes conflicts removed
    
    * minor routing updates
    
    * updating css for index
    
    * minor index.js refactor

--- public/js/index.js
+++ public/js/index.js
@@ -1,171 +1,12 @@
-// // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
 // ------------- MAPS CODE STARTS ------------------- //
 
   
 var dbRestaurants = [];
 
-function getDbRestaurants() {
-  $.get("/api/restaurants", function(data) {
-    dbRestaurants = data;
-    console.log(data);
-    console.log(dbRestaurants[0].latitude);
-  });
-}
-
-getDbRestaurants();
-
-
 var map;
 var InforObj = [];
 var centerCords = { lat: 36.1627, lng: -86.7816 };
 
-// Data for the markers
-var restaurants = [
-  {
-    name: "Slow Burn",
-    address: "x",
-    latitude: 36.27877,
-    longitude: -86.69017,
-    phone: 615 - 615 - 6115,
-    website: "x",
-    facebook: "x"
-  },
-  {
-    name: "Prince's Hot Chicken Shack",
-    address: "x",
-    latitude: 36.22998,
-    longitude: -86.76077,
-    phone: 615 - 615 - 6115,
-    website: "x",
-    facebook: "x"
-  },
-  {
-    name: "Prince's Hot Chicken Shack-South",
-    address: "x",
-    latitude: 36.04371,
-    longitude: -86.71239,
-    phone: 615 - 615 - 6115,
-    website: "x",
-    facebook: "x"
-  },
-  {
-    name: "Scoreboard Bar & Grill",
-    address: "x",
-    latitude: 36.22074,
-    longitude: -86.69386,
-    phone: 615 - 615 - 6115,
-    website: "x",
-    facebook: "x"
-  },
-  {
-    name: "400 Degrees Hot Chicken",
-    address: "x",
-    latitude: 36.19855,
-    longitude: -86.83752,
-    phone: 615 - 615 - 6115,
-    website: "x",
-    facebook: "x"
-  }
-];
 
 function setMarkers() {
   // add custom marker image
@@ -261,13 +102,20 @@ function closeOtherInfo() {
 function initMap() {
   console.log("running");
 
-  // The map, centered on Nashville
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: centerCords,
-    zoom: 10
-  });
+  $.get("/api/restaurants", function(data) {
+    dbRestaurants = data;
+    console.log(data);
+
+
+    // The map, centered on Nashville
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: centerCords,
+      zoom: 10
+    });
 
   // console.log("the map instance");
   // console.log(map);
   setMarkers();
+});
+
 }
